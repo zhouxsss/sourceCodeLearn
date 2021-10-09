a4d47f6cee06ecf50c9214429d4de2fcce62604b
zhouxs <zhouxs@yunphant.com>
2021-10-09 10:42:25 +0800

    koa-compose

--- koa-compose/index.js
+++ koa-compose/index.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 /**
  * Expose compositor.
  */
 
-module.exports = compose
+module.exports = compose;
 
 /**
  * Compose `middleware` returning
@@ -16,10 +16,13 @@ module.exports = compose
  * @api public
  */
 
-function compose (middleware) {
-  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+function compose(middleware) {
+  if (!Array.isArray(middleware))
+    throw new TypeError('Middleware stack must be an array!');
+  // 接收一个参数，校验参数是数组，且校验数组中的每一项是函数。
   for (const fn of middleware) {
-    if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+    if (typeof fn !== 'function')
+      throw new TypeError('Middleware must be composed of functions!');
   }
 
   /**
@@ -28,21 +31,24 @@ function compose (middleware) {
    * @api public
    */
 
+  // 返回一个函数，这个函数接收两个参数，分别是context和next，这个函数最后返回Promise
   return function (context, next) {
     // last called middleware #
-    let index = -1
-    return dispatch(0)
-    function dispatch (i) {
-      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
-      index = i
-      let fn = middleware[i]
-      if (i === middleware.length) fn = next
-      if (!fn) return Promise.resolve()
+    let index = -1;
+    return dispatch(0);
+    function dispatch(i) {
+      if (i <= index)
+        return Promise.reject(new Error('next() called multiple times'));
+      index = i;
+      let fn = middleware[i];
+      if (i === middleware.length) fn = next;
+      if (!fn) return Promise.resolve();
       try {
-        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)))
+        // dispatch.bind(null, i + 1)) 取下一个函数，即传入的 next
+        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
       } catch (err) {
-        return Promise.reject(err)
+        return Promise.reject(err);
       }
     }
-  }
+  };
 }
--- koa-compose/test/test.js
+++ koa-compose/test/test.js
@@ -1,328 +1,342 @@
-'use strict'
+'use strict';
 
 /* eslint-env jest */
 
-const compose = require('..')
-const assert = require('assert')
+const compose = require('..');
+const assert = require('assert');
 
-function wait (ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms || 1))
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms || 1));
 }
 
-function isPromise (x) {
-  return x && typeof x.then === 'function'
+function isPromise(x) {
+  return x && typeof x.then === 'function';
 }
 
 describe('Koa Compose', function () {
   it('should work', async () => {
-    const arr = []
-    const stack = []
+    const arr = [];
+    const stack = [];
 
     stack.push(async (context, next) => {
-      arr.push(1)
-      await wait(1)
-      await next()
-      await wait(1)
-      arr.push(6)
-    })
+      arr.push(1);
+      await wait(1);
+      await next();
+      await wait(1);
+      arr.push(6);
+    });
 
     stack.push(async (context, next) => {
-      arr.push(2)
-      await wait(1)
-      await next()
-      await wait(1)
-      arr.push(5)
-    })
+      arr.push(2);
+      await wait(1);
+      await next();
+      await wait(1);
+      arr.push(5);
+    });
 
     stack.push(async (context, next) => {
-      arr.push(3)
-      await wait(1)
-      await next()
-      await wait(1)
-      arr.push(4)
-    })
+      arr.push(3);
+      await wait(1);
+      await next();
+      await wait(1);
+      arr.push(4);
+    });
 
-    await compose(stack)({})
-    expect(arr).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]))
-  })
+    await compose(stack)({});
+    expect(arr).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
+  });
 
   it('should be able to be called twice', () => {
-    const stack = []
+    const stack = [];
 
     stack.push(async (context, next) => {
-      context.arr.push(1)
-      await wait(1)
-      await next()
-      await wait(1)
-      context.arr.push(6)
-    })
+      context.arr.push(1);
+      await wait(1);
+      await next();
+      await wait(1);
+      context.arr.push(6);
+    });
 
     stack.push(async (context, next) => {
-      context.arr.push(2)
-      await wait(1)
-      await next()
-      await wait(1)
-      context.arr.push(5)
-    })
+      context.arr.push(2);
+      await wait(1);
+      await next();
+      await wait(1);
+      context.arr.push(5);
+    });
 
     stack.push(async (context, next) => {
-      context.arr.push(3)
-      await wait(1)
-      await next()
-      await wait(1)
-      context.arr.push(4)
-    })
+      context.arr.push(3);
+      await wait(1);
+      await next();
+      await wait(1);
+      context.arr.push(4);
+    });
 
-    const fn = compose(stack)
-    const ctx1 = { arr: [] }
-    const ctx2 = { arr: [] }
-    const out = [1, 2, 3, 4, 5, 6]
+    const fn = compose(stack);
+    const ctx1 = { arr: [] };
+    const ctx2 = { arr: [] };
+    const out = [1, 2, 3, 4, 5, 6];
 
-    return fn(ctx1).then(() => {
-      assert.deepEqual(out, ctx1.arr)
-      return fn(ctx2)
-    }).then(() => {
-      assert.deepEqual(out, ctx2.arr)
-    })
-  })
+    return fn(ctx1)
+      .then(() => {
+        assert.deepEqual(out, ctx1.arr);
+        return fn(ctx2);
+      })
+      .then(() => {
+        assert.deepEqual(out, ctx2.arr);
+      });
+  });
 
   it('should only accept an array', () => {
-    expect(() => compose()).toThrow(TypeError)
-  })
+    expect(() => compose()).toThrow(TypeError);
+  });
 
   it('should create next functions that return a Promise', function () {
-    const stack = []
-    const arr = []
+    const stack = [];
+    const arr = [];
     for (let i = 0; i < 5; i++) {
       stack.push((context, next) => {
-        arr.push(next())
-      })
+        arr.push(next());
+      });
     }
 
-    compose(stack)({})
+    compose(stack)({});
 
     for (const next of arr) {
-      assert(isPromise(next), 'one of the functions next is not a Promise')
+      assert(isPromise(next), 'one of the functions next is not a Promise');
     }
-  })
+  });
 
   it('should work with 0 middleware', function () {
-    return compose([])({})
-  })
+    return compose([])({});
+  });
 
   it('should only accept middleware as functions', () => {
-    expect(() => compose([{}])).toThrow(TypeError)
-  })
+    expect(() => compose([{}])).toThrow(TypeError);
+  });
 
   it('should work when yielding at the end of the stack', async () => {
-    const stack = []
-    let called = false
+    const stack = [];
+    let called = false;
 
     stack.push(async (ctx, next) => {
-      await next()
-      called = true
-    })
+      await next();
+      called = true;
+    });
 
-    await compose(stack)({})
-    assert(called)
-  })
+    await compose(stack)({});
+    assert(called);
+  });
 
   it('should reject on errors in middleware', () => {
-    const stack = []
+    const stack = [];
 
-    stack.push(() => { throw new Error() })
+    stack.push(() => {
+      throw new Error();
+    });
 
-    return compose(stack)({})
-      .then(() => {
-        throw new Error('promise was not rejected')
-      }, (e) => {
-        expect(e).toBeInstanceOf(Error)
-      })
-  })
+    return compose(stack)({}).then(
+      () => {
+        throw new Error('promise was not rejected');
+      },
+      (e) => {
+        expect(e).toBeInstanceOf(Error);
+      }
+    );
+  });
 
   it('should keep the context', () => {
-    const ctx = {}
+    const ctx = {};
 
-    const stack = []
-
-    stack.push(async (ctx2, next) => {
-      await next()
-      expect(ctx2).toEqual(ctx)
-    })
+    const stack = [];
 
     stack.push(async (ctx2, next) => {
-      await next()
-      expect(ctx2).toEqual(ctx)
-    })
+      await next();
+      expect(ctx2).toEqual(ctx);
+    });
 
     stack.push(async (ctx2, next) => {
-      await next()
-      expect(ctx2).toEqual(ctx)
-    })
+      await next();
+      expect(ctx2).toEqual(ctx);
+    });
 
-    return compose(stack)(ctx)
-  })
+    stack.push(async (ctx2, next) => {
+      await next();
+      expect(ctx2).toEqual(ctx);
+    });
+
+    return compose(stack)(ctx);
+  });
 
   it('should catch downstream errors', async () => {
-    const arr = []
-    const stack = []
+    const arr = [];
+    const stack = [];
 
     stack.push(async (ctx, next) => {
-      arr.push(1)
+      arr.push(1);
       try {
-        arr.push(6)
-        await next()
-        arr.push(7)
+        arr.push(6);
+        await next();
+        arr.push(7);
       } catch (err) {
-        arr.push(2)
+        arr.push(2);
       }
-      arr.push(3)
-    })
+      arr.push(3);
+    });
 
     stack.push(async (ctx, next) => {
-      arr.push(4)
-      throw new Error()
-    })
+      arr.push(4);
+      throw new Error();
+    });
 
-    await compose(stack)({})
-    expect(arr).toEqual([1, 6, 4, 2, 3])
-  })
+    await compose(stack)({});
+    expect(arr).toEqual([1, 6, 4, 2, 3]);
+  });
 
   it('should compose w/ next', () => {
-    let called = false
+    let called = false;
 
     return compose([])({}, async () => {
-      called = true
+      called = true;
     }).then(function () {
-      assert(called)
-    })
-  })
+      assert(called);
+    });
+  });
 
   it('should handle errors in wrapped non-async functions', () => {
-    const stack = []
+    const stack = [];
 
     stack.push(function () {
-      throw new Error()
-    })
+      throw new Error();
+    });
 
-    return compose(stack)({}).then(() => {
-      throw new Error('promise was not rejected')
-    }, (e) => {
-      expect(e).toBeInstanceOf(Error)
-    })
-  })
+    return compose(stack)({}).then(
+      () => {
+        throw new Error('promise was not rejected');
+      },
+      (e) => {
+        expect(e).toBeInstanceOf(Error);
+      }
+    );
+  });
 
   // https://github.com/koajs/compose/pull/27#issuecomment-143109739
   it('should compose w/ other compositions', () => {
-    const called = []
+    const called = [];
 
     return compose([
       compose([
         (ctx, next) => {
-          called.push(1)
-          return next()
+          called.push(1);
+          return next();
         },
         (ctx, next) => {
-          called.push(2)
-          return next()
-        }
+          called.push(2);
+          return next();
+        },
       ]),
       (ctx, next) => {
-        called.push(3)
-        return next()
-      }
-    ])({}).then(() => assert.deepEqual(called, [1, 2, 3]))
-  })
+        called.push(3);
+        return next();
+      },
+    ])({}).then(() => assert.deepEqual(called, [1, 2, 3]));
+  });
 
   it('should throw if next() is called multiple times', () => {
     return compose([
       async (ctx, next) => {
-        await next()
-        await next()
+        await next();
+        await next();
+      },
+    ])({}).then(
+      () => {
+        throw new Error('boom');
+      },
+      (err) => {
+        assert(/multiple times/.test(err.message));
       }
-    ])({}).then(() => {
-      throw new Error('boom')
-    }, (err) => {
-      assert(/multiple times/.test(err.message))
-    })
-  })
+    );
+  });
 
   it('should return a valid middleware', () => {
-    let val = 0
+    let val = 0;
     return compose([
       compose([
         (ctx, next) => {
-          val++
-          return next()
+          val++;
+          return next();
         },
         (ctx, next) => {
-          val++
-          return next()
-        }
+          val++;
+          return next();
+        },
       ]),
       (ctx, next) => {
-        val++
-        return next()
-      }
+        val++;
+        return next();
+      },
     ])({}).then(function () {
-      expect(val).toEqual(3)
-    })
-  })
+      expect(val).toEqual(3);
+    });
+  });
 
   it('should return last return value', () => {
-    const stack = []
+    const stack = [];
 
     stack.push(async (context, next) => {
-      const val = await next()
-      expect(val).toEqual(2)
-      return 1
-    })
+      const val = await next();
+      expect(val).toEqual(2);
+      return 1;
+    });
 
     stack.push(async (context, next) => {
-      const val = await next()
-      expect(val).toEqual(0)
-      return 2
-    })
+      const val = await next();
+      expect(val).toEqual(0);
+      return 2;
+    });
 
-    const next = () => 0
+    const next = () => 0;
     return compose(stack)({}, next).then(function (val) {
-      expect(val).toEqual(1)
-    })
-  })
+      expect(val).toEqual(1);
+    });
+  });
 
   it('should not affect the original middleware array', () => {
-    const middleware = []
+    const middleware = [];
     const fn1 = (ctx, next) => {
-      return next()
-    }
-    middleware.push(fn1)
+      return next();
+    };
+    middleware.push(fn1);
 
     for (const fn of middleware) {
-      assert.equal(fn, fn1)
+      assert.equal(fn, fn1);
     }
 
-    compose(middleware)
+    compose(middleware);
 
     for (const fn of middleware) {
-      assert.equal(fn, fn1)
+      assert.equal(fn, fn1);
     }
-  })
+  });
 
   it('should not get stuck on the passed in next', () => {
-    const middleware = [(ctx, next) => {
-      ctx.middleware++
-      return next()
-    }]
+    const middleware = [
+      (ctx, next) => {
+        ctx.middleware++;
+        return next();
+      },
+    ];
     const ctx = {
       middleware: 0,
-      next: 0
-    }
+      next: 0,
+    };
 
     return compose(middleware)(ctx, (ctx, next) => {
-      ctx.next++
-      return next()
+      ctx.next++;
+      return next();
     }).then(() => {
-      expect(ctx).toEqual({ middleware: 1, next: 1 })
-    })
-  })
-})
+      expect(ctx).toEqual({ middleware: 1, next: 1 });
+    });
+  });
+});
